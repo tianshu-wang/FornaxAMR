e7ffc27eae94c4c2f75ee02a7757db9ff5988623
tianshu-wang <wangts1997@gmail.com>
2026-04-12 17:37:10 -0700

    fix stellar compilation

--- machines/stellar-intel.mk
+++ machines/stellar-intel.mk
@@ -2,5 +2,7 @@
 # Converted from machines_tmp/princeton/stellar-intel.mk.
 
 CC := h5pcc
+HDF5_CFLAGS :=
+HDF5_LIBS :=
 MACHINE_CFLAGS := -xCORE-AVX512 -diag-disable=161,3180
 MACHINE_LDLIBS := -lgsl -mkl -lifcore -lmpi -ldl -lrt -lpthread
--- src/prj_gravity.c
+++ src/prj_gravity.c
@@ -2,6 +2,10 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
 #include "prj.h"
 
 #if defined(PRJ_ENABLE_MPI)
